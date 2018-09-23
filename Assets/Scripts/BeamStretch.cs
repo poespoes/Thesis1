3eb1fcc01ef991d43806ef11063532336d02997e
Mostafa Haque <93haque@gmail.com>
2018-09-23 09:23:49 -0400

    Errors fixed

---
+++ Assets/Scripts/BeamStretch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamStretch : MonoBehaviour {
+
+    public GameObject lightBeam;
+
+	// Use this for initialization
+	void Start () {
+        lightBeam = this.gameobject.transform.Find("MimLight");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 thisPos = this.transform.position;
+
+        float distance = Vector3.Distance(mousePos, thisPos);
+
+        if (Input.GetMouseButtonDown(1)) {
+            //toggle light on/off
+            Debug.log(distance);
+           
+        }
+    }
+}
