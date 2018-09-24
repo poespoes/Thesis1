bf24981d9006212a096e8aafcef79f2f97afb737
Mostafa Haque <93haque@gmail.com>
2018-09-24 02:30:07 -0400

    Cinemachine Camera working

---
+++ Assets/Scripts/GameState/playStateActivate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class playStateActivate : MonoBehaviour {
+	
+	public PlayableDirector director;
+
+	// Use this for initialization
+	void Start ()
+	{
+		director = this.GetComponent<PlayableDirector>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log("This clip is " + director.state);
+
+		String playableState = director.state.ToString();
+
+		if (playableState == "Paused")
+		{
+			Debug.Log("It has stopped playing DO THE THING");
+		}
+
+	}
+}
