47416cfe51fe6dfc6409282947e3dbf0f5a08e4d
Mostafa Haque <93haque@gmail.com>
2018-10-02 19:30:00 -0400

    Intro script

---
+++ Assets/Scripts/EnvironmentalScript/Intro/introScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introScript : MonoBehaviour
+{
+	public Animator handAnimator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.tag == "MimLight") {
+			handAnimator.SetBool("willShake",true);
+			Debug.Log("Shaken not stirred");
+		}
+	}
+}
