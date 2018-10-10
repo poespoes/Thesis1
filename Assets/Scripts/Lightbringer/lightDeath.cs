e980ba390f397fa6a8c5282741b32722dd611553
Mostafa Haque <93haque@gmail.com>
2018-10-10 12:58:31 -0400

    Timer script for light death

--- Assets/Scripts/Lightbringer/lightDeath.cs
+++ Assets/Scripts/Lightbringer/lightDeath.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class lightDeath : MonoBehaviour {
+public class lightDeath : MonoBehaviour
+{
+	public float timer;
+	public float timeRemaining;
 
+	public Image redOutPanel;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		timeRemaining = timer;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		//timer = timer - Time.deltaTime;
+		//Debug.Log(timer + " seconds remaining");
+
+		if (this.GetComponent<BeamController>().isOn == true)
+		{
+			timeRemaining = timeRemaining - Time.deltaTime;
+		}
+		else
+		{
+			if (timeRemaining < timer)
+			{
+				timeRemaining = timeRemaining + Time.deltaTime;
+			}
+		}
+		
+		//Debug.Log(timeRemaining + " seconds remaining" );
+		
 		
 	}
 }
