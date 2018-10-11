260435f7a14a09511dfb99d5687c626dd80bd16b
Unknown <poe_s@hotmail.com>
2018-10-11 13:22:51 -0400

    Alpha scene and script

---
+++ Assets/Scripts/TestScripts/AlphaTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaTest : MonoBehaviour {
+
+    public Color A = Color.magenta;
+    public Color B = Color.blue;
+    public float speed = 1.0f;
+    public float lerpProgress = 0;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            lerpProgress += 0.01f;
+            Debug.Log("Bumping up!");
+        }
+
+
+        spriteRenderer.color = Color.Lerp(A, B, lerpProgress);
+    }
+}
