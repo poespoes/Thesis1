b02b998c06f8906246fdb826773ad7187cdea601
Mostafa Haque <93haque@gmail.com>
2018-10-16 18:07:55 -0400

    Glowworm camera follow script

---
+++ Assets/Scripts/EnvironmentalScript/glowworm/glowWormScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinemachine.Examples
+{
+	public class glowWormScript : MonoBehaviour
+	{
+
+		public CinemachineVirtualCamera playerCam;
+		public GameObject player;
+		public gameState gameManager;
+
+		// Use this for initialization
+		void Start()
+		{
+			playerCam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
+			player = GameObject.Find("Player");
+			gameManager = GameObject.Find("GameManager").GetComponent<gameState>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.transform.tag == "MimLight")
+			{
+				playerCam.m_LookAt = this.transform;
+				playerCam.m_Follow = this.transform;
+
+				this.GetComponent<Animator>().SetBool("hasTriggered", true);
+				gameManager.interactive = false;
+			}
+		}
+
+		public void endMe()
+		{
+			playerCam.m_LookAt = player.transform;
+			playerCam.m_Follow = player.transform;
+			Destroy(this.gameObject);
+			gameManager.interactive = true;
+		}
+	}
+}
