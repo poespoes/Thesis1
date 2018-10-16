6a4b9a7d2b1bfd2a20bebabfee7316aa32e9b6ca
Mostafa Haque <93haque@gmail.com>
2018-10-16 16:50:59 -0400

    Light activated stuff

---
+++ Assets/Scripts/EnvironmentalScript/glowworm/glowwormActivate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinemachine.Examples
+{
+	public class glowwormActivate : MonoBehaviour
+	{
+
+		public string animBool;
+		public CinemachineVirtualCamera playerCam;
+
+
+		// Use this for initialization
+		void Start()
+		{
+			playerCam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject.tag == "MimLight")
+			{
+				Debug.Log("Worms are lit");
+				transform.parent.GetComponent<Animator>().SetBool(animBool,true);
+
+				playerCam.m_LookAt = this.transform;
+				playerCam.m_Follow = this.transform;
+			}
+		}
+
+	
+	}
+}
---
+++ Assets/Scripts/EnvironmentalScript/glowworm/glowwormDeath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinemachine.Examples
+{
+	public class glowwormDeath : MonoBehaviour
+	{
+
+		public GameObject childOne;
+		public GameObject childTwo;
+		public GameObject player;
+		public CinemachineVirtualCamera cvcam;
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		void killFirstChild()
+		{
+			Destroy(childOne);
+			cvcam.m_Follow = player.transform;
+		}
+
+		void killSecondChild()
+		{
+			Destroy(childTwo);
+			cvcam.m_Follow = player.transform;
+		}
+	}
+}
