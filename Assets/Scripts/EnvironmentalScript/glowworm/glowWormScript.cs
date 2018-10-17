74fd9ecd453fa28552936958ca0cdf2b799d4d4f
Mostafa Haque <93haque@gmail.com>
2018-10-16 21:30:59 -0400

    Backup level push

--- Assets/Scripts/EnvironmentalScript/glowworm/glowWormScript.cs
+++ Assets/Scripts/EnvironmentalScript/glowworm/glowWormScript.cs
@@ -36,6 +36,8 @@ namespace Cinemachine.Examples
 				gameManager.interactive = false;
 
 				playerCam.m_Lens.OrthographicSize = playerCam.m_Lens.OrthographicSize + 10;
+				player.GetComponent<BeamStretch>().minStretch = playerCam.m_Lens.OrthographicSize;
+				player.GetComponent<BeamStretch>().maxStretch = playerCam.m_Lens.OrthographicSize+ + 5;
 			}
 		}
 
