6124aa2a611ac74495eab302a32db5cdfab21e17
Mostafa Haque <93haque@gmail.com>
2018-11-12 10:51:10 -0500

    Basic sanity meter implemented

---
+++ Assets/Scripts/Overimagination/Sanity.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sanity : MonoBehaviour
+{
+
+	public float sanity;
+	public float maxSanity;
+	
+	public Player playerComp;
+	public PlayerAnimation playerAnim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerComp = this.GetComponent<Player>();
+		playerAnim = this.GetComponent<PlayerAnimation>();
+		if (sanity == null)
+		{
+			sanity = 10;
+		}
+		maxSanity = sanity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		sanity = Mathf.Clamp(sanity, 0, maxSanity);
+
+		if (playerComp.isLIt == false)
+		{
+			sanity -= Time.deltaTime;
+			sanity = Mathf.Clamp(sanity, 0, maxSanity);
+			
+		}
+		else
+		{
+			sanity += Time.deltaTime;
+			sanity = Mathf.Clamp(sanity, 0, maxSanity);
+		}
+		
+	}
+}
