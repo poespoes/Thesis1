f4d9fa51ea3d20b9fe9f8f792abb51dbe21c37ea
Mostafa Haque <93haque@gmail.com>
2018-11-14 18:17:44 -0500

    CameraShake broken

---
+++ Assets/Scripts/camerShake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerShake : MonoBehaviour
+{
+	public float shakeRange;
+	public Vector2 durationRange;
+
+	float shakeOffset = 0;
+	float shakeTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.T))
+		{
+			BeamShake();
+		}
+	}
+	
+	void BeamShake() {
+		shakeTimer -= Time.deltaTime;
+		if (shakeTimer < 0) {
+			shakeTimer += Random.Range(durationRange.x, durationRange.y);
+			if (shakeOffset > 0)
+				shakeOffset = -shakeRange;
+			else
+				shakeOffset = shakeRange;
+		}
+
+		Vector3 euler = transform.eulerAngles;
+		euler.z += shakeOffset;
+		transform.eulerAngles = euler;
+	}
+}
