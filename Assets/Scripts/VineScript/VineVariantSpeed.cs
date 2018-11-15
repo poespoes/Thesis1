ca0c6e812c01de7949b18a9d116ebb8707c7a90b
Nobo Bhowmik <nobonita.bhowmik@outlook.com>
2018-11-14 19:56:57 -0500

    HL edited/duplicate vines

---
+++ Assets/Scripts/VineScript/VineVariantSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineVariantSpeed : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        if (speed == null)
+        {
+            speed = 1;
+        }
+
+        this.GetComponent<Animator>().speed = speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
