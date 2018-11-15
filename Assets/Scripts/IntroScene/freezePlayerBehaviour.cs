d92e5bffe7e82cc87c865654892d60ff841b3aeb
Mostafa Haque <93haque@gmail.com>
2018-11-14 21:10:01 -0500

    MimStay

---
+++ Assets/Scripts/IntroScene/freezePlayerBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class freezePlayerBehaviour : MonoBehaviour
+{
+
+	public float holdtimer;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("Player Frozen");
+			Freeze(other.gameObject);
+		}
+	}
+
+	void Freeze(GameObject other)
+	{
+		GameObject.Find("GameManager").GetComponent<gameState>().interactive = false;
+		other.GetComponent<PlayerAnimation>().WalkingState = 0;
+		other.GetComponent<Player>().isWalking = false;
+		other.GetComponent<Animator>().Play("Idle");
+		Invoke("UnFreeze",holdtimer);
+	}
+
+	void UnFreeze()
+	{
+		GameObject.Find("GameManager").GetComponent<gameState>().interactive = true;
+	}
+}
