ee5a3015b0181ee62b223336ecca00901d01d060
Mostafa Haque <93haque@gmail.com>
2018-11-15 21:46:50 -0500

    CheckpointWorking

---
+++ Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+		float posX = GameObject.Find("CheckPointManager").GetComponent<GameSingleTon>().lastCheckPoint.x;
+		float posY = GameObject.Find("CheckPointManager").GetComponent<GameSingleTon>().lastCheckPoint.y;
+		
+		this.transform.position = new Vector3(posX, posY, this.transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
