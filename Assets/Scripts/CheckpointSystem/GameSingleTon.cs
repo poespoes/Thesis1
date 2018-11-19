c45a9f6ef9da65aad018245fa24e1d32e2b0c1d3
Mostafa Haque <93haque@gmail.com>
2018-11-19 10:21:03 -0500

    Temporary Checkpoint Solution

---
+++ Assets/Scripts/CheckpointSystem/GameSingleTon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSingleTon : MonoBehaviour
+{
+
+	private GameSingleTon instance;
+	public Vector2 lastCheckPoint;
+	public Vector2 initialCheckPoint;
+
+	// Use this for initialization
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(instance);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+		lastCheckPoint = initialCheckPoint;
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
