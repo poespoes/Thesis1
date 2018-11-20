3c92aff5920acfb73eecd83ef41df8dcc71e5367
Mostafa Haque <93haque@gmail.com>
2018-11-20 12:48:42 -0500

    Cinemachine Camera Shake working

---
+++ Assets/Scripts/CinemachineCameraShake.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinemachine.Examples
+{
+	public class CinemachineCameraShake : MonoBehaviour
+	{
+
+		public GameObject _gameManager;
+		public CameraControl _cameraControl;
+		public CinemachineVirtualCamera _virtualCamera;
+
+		public float amplitudeGain;
+		public float frequencyGain;
+		
+
+		// Use this for initialization
+		void Start()
+		{
+			_gameManager  = GameObject.Find("GameManager");
+			_cameraControl = _gameManager.GetComponent<CameraControl>();
+			
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			_virtualCamera = _cameraControl.focusedCamera;
+
+			if (Input.GetKey(KeyCode.F))
+			{
+				Shake();
+			}
+			else
+			{
+				UnShake();
+			}
+
+		}
+
+		void Shake()
+		{
+			_virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitudeGain;
+			_virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequencyGain;
+		}
+		
+		void UnShake()
+		{
+			_virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
+			_virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 0;
+		}
+	}
+}
