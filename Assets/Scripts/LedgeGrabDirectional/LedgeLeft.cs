d29d45b0c054f8da42a6057276869bb58b28e6ff
Mostafa Haque <93haque@gmail.com>
2018-12-04 13:01:32 -0500

    Ledge Grab Side fixed

---
+++ Assets/Scripts/LedgeGrabDirectional/LedgeLeft.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeLeft : MonoBehaviour
+{
+	private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		player = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Horizontal")>0||player.GetComponent<Rigidbody2D>().velocity.x>0)
+		{
+			this.GetComponent<LedgeGrab>().enabled = false;
+		}
+		else
+		{
+			this.GetComponent<LedgeGrab>().enabled = true;
+		}	
+	}
+}
---
+++ Assets/Scripts/LedgeGrabDirectional/LedgeRight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeRight : MonoBehaviour
+{
+	private GameObject player;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		player = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Horizontal")<0||player.GetComponent<Rigidbody2D>().velocity.x<0)
+		{
+			this.GetComponent<LedgeGrab>().enabled = false;
+		}
+		else
+		{
+			this.GetComponent<LedgeGrab>().enabled = true;
+		}	
+	}
+}
