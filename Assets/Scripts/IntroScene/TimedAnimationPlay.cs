16e0ddeef64aec55e29491e32bfb72153bb48031
Mostafa Haque <93haque@gmail.com>
2018-12-09 19:29:22 -0500

    TimedAnimator Script

---
+++ Assets/Scripts/IntroScene/TimedAnimationPlay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedAnimationPlay : MonoBehaviour
+{
+
+	public Animator animPlayer;
+	public AnimationClip animClip;
+	public float timer;
+
+	public int animClipArraySize;
+	public AnimationClip[] animClipArray;
+
+	public string boolName;
+	public bool callItself;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (callItself = true)
+		{
+			Initiate();
+			callItself = false;
+		}
+	}
+
+	public void Initiate()
+	{
+		Invoke("PlayAfterTime",timer);
+	}
+
+	public void PlayAfterTime()
+	{
+		animPlayer.Play(animClip.name);
+		animPlayer.SetBool(boolName,true);
+	}
+}
