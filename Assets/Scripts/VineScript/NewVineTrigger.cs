413492180713ff0db588db12a9c0c806615c7fec
Mostafa Haque <93haque@gmail.com>
2018-12-11 14:28:11 -0500

    Vine Chase and MimLight both in

--- Assets/Scripts/VineScript/NewVineTrigger.cs
+++ Assets/Scripts/VineScript/NewVineTrigger.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewVineTrigger : MonoBehaviour {
+public class NewVineTrigger : MonoBehaviour
+{
+	public VineChaseLight parentVine;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		parentVine = this.transform.parent.GetComponent<VineChaseLight>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			parentVine.inRange = true;
+		}
+		
+	
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			parentVine.inRange = false;
+		}
 	}
 }
--- Assets/Scripts/VineScript/VineChaseLight.cs
+++ Assets/Scripts/VineScript/VineChaseLight.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class VineChaseLight : MonoBehaviour {
+public class VineChaseLight : MonoBehaviour
+{
+	public bool inRange;
+	public bool isLit;
+
+	public float time;
+
+	public Transform destination;
+	
+	
 
 	// Use this for initialization
 	void Start () {
-		
+		if (time == 0)
+		{
+			time = 5;
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		
+		
+		isLit = GameObject.Find("Player").GetComponent<Player>().isLIt;
+
+		if (inRange == true && isLit == true)
+		{
+			this.transform.DOMove(destination.transform.position, time);
+		}
+		
 		
 	}
 }
