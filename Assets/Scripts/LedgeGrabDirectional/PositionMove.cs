918b6707385096d4f66b0d3e1582fcbb76d72520
Mostafa Haque <93haque@gmail.com>
2018-12-12 16:07:44 -0500

    new ledge grab working with camera bug

---
+++ Assets/Scripts/LedgeGrabDirectional/PositionMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionMove : MonoBehaviour
+{
+
+	public Transform left;
+	public Transform right;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player").gameObject;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Horizontal")<0||player.GetComponent<Rigidbody2D>().velocity.x<0)
+		{
+			this.transform.position = left.transform.position;
+		}
+		
+		if (Input.GetAxisRaw("Horizontal")>0||player.GetComponent<Rigidbody2D>().velocity.x>0)
+		{
+			this.transform.position = right.transform.position;
+		}
+	}
+}
