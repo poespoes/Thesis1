e8018bfd8c6f29d91cd28b5415588376a25b0a6e
Mostafa Haque <93haque@gmail.com>
2018-12-12 22:02:50 -0500

    Milestone and Intro Scene linked

---
+++ Assets/Scenes/CutsceneScripts/sceneTransition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransition : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LoadScene()
+	{
+		//SceneManager.LoadScene(nextScene.name);
+
+		int i = SceneManager.GetActiveScene().buildIndex + 1;
+		
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
