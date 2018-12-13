c6fdec208761d40c9d50116b6a9fb496a7cd8b60
Mostafa Haque <93haque@gmail.com>
2018-12-12 23:37:07 -0500

    Worm delay and icon

---
+++ Assets/Scripts/CutsceneScripts/OneOffScripts/MimLightEmoji.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimLightEmoji : MonoBehaviour
+{
+	public Animator MimEmoji;
+
+	// Use this for initialization
+	void Start ()
+	{
+		MimEmoji = GameObject.Find("MimEmoji").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartEmojiHint()
+	{
+		Invoke("MimEmojiHint", 5);
+	}
+
+	void MimEmojiHint()
+	{
+		MimEmoji.SetBool("MimTurnOnLight",true);
+	}
+}
---
+++ Assets/Scripts/CutsceneScripts/OneOffScripts/VineSpawnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineSpawnScript : MonoBehaviour
+{
+	public float spawnDelay;
+	public GameObject objectToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		objectToSpawn.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartSpawn()
+	{
+		Invoke("Spawn",spawnDelay);
+	}
+
+	public void Spawn()
+	{
+		objectToSpawn.SetActive(true);
+	}
+}
